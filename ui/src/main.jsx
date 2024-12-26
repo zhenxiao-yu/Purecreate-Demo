1731b1a99ca6e4fbd3e348a37772fe7f9b926ab3
Mark Yu · 于震潇 <zyu347@uwo.ca>
2024-12-26 14:03:45 -0500

    added comments

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
+// Create the root for React application and render the App component
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
