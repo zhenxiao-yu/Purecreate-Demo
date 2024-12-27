839ed3362f5b7949d56f8ce243c8e134b239c7a1
Mark Yu · 于震潇 <zyu347@uwo.ca>
2024-12-26 23:46:10 -0500

    asset clean up

--- ui/src/assets/index.js
+++ ui/src/assets/index.js
@@ -1,20 +1,45 @@
-import fileIcon from "./file.png";
-import swatch from "./swatch.png";
-import download from "./download.png";
-import ai from "./ai.png";
+// src/assets/index.js
 
-import logoShirt from "./logo-tshirt.png";
-import stylishShirt from "./stylish-tshirt.png";
-import logoControls from "./logo-controls.png";
-import textIcon from "./text-icon.png";
-import textureLogoPicker from "./textureLogoPicker.jpeg";
-import texture1 from "./texture1.jpeg";
-import texture2 from "./texture2.jpeg";
-import texture3 from "./texture3.jpeg";
-import texture4 from "./texture4.jpeg";
-import texture5 from "./texture5.jpeg";
-import logo1 from "./logo1.png";
-import logo2 from "./logo2.png";
+// Icons
+import fileIcon from "./icons/file.png";
+import swatch from "./icons/swatch.png";
+import download from "./icons/download.png";
+import ai from "./icons/ai.png";
+import logoControls from "./icons/logo-controls.png";
+import textIcon from "./icons/text-icon.png";
 
+// Shirts
+import logoShirt from "./shirts/logo-tshirt.png";
+import stylishShirt from "./shirts/stylish-tshirt.png";
 
-export { ai, fileIcon, swatch, download, logoShirt, stylishShirt, logoControls, textIcon, textureLogoPicker, texture1, texture2, texture3, texture4, texture5, logo1, logo2 };
+// Textures
+import textureLogoPicker from "./textures/textureLogoPicker.jpeg";
+import texture1 from "./textures/texture1.jpeg";
+import texture2 from "./textures/texture2.jpeg";
+import texture3 from "./textures/texture3.jpeg";
+import texture4 from "./textures/texture4.jpeg";
+import texture5 from "./textures/texture5.jpeg";
+
+// Logos
+import logo1 from "./logos/logo1.png";
+import logo2 from "./logos/logo2.png";
+
+// Export All
+export {
+    fileIcon,
+    swatch,
+    download,
+    ai,
+    logoControls,
+    textIcon,
+    logoShirt,
+    stylishShirt,
+    textureLogoPicker,
+    texture1,
+    texture2,
+    texture3,
+    texture4,
+    texture5,
+    logo1,
+    logo2,
+};
