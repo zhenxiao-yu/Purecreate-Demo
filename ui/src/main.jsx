415c677cc7987301fdbd66eab3e2e667df7554b9
Mark Yu · 于震潇 <zyu347@uwo.ca>
2024-12-29 17:47:12 -0500

    added normalise.css
    a

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './normalize.css'
+import { Analytics } from '@vercel/analytics/react';
 
 // Create the root for React application and render the App component
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
+        <Analytics mode={'production'} />
     </React.StrictMode>
 );
