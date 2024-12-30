7e88bda54887b5c9769d47e7426bbcebea6d0045
Mark Yu · 于震潇 <zyu347@uwo.ca>
2024-12-29 22:39:11 -0500

    assets store update

--- ui/src/assets/index.js
+++ ui/src/assets/index.js
@@ -19,10 +19,22 @@ import texture2 from "./textures/texture2.jpeg";
 import texture3 from "./textures/texture3.jpeg";
 import texture4 from "./textures/texture4.jpeg";
 import texture5 from "./textures/texture5.jpeg";
+import texture6 from "./textures/texture6.jpeg";
+import texture7 from "./textures/texture7.jpeg";
+import texture8 from "./textures/texture8.jpeg";
+import texture9 from "./textures/texture9.jpeg";
+import texture10 from "./textures/texture10.jpeg";
 
 // Logos
 import logo1 from "./logos/logo1.png";
 import logo2 from "./logos/logo2.png";
+import logo3 from "./logos/logo3.png";
+import logo4 from "./logos/logo4.png";
+import logo5 from "./logos/logo5.png";
+import logo6 from "./logos/logo6.png";
+import logo7 from "./logos/logo7.png";
+import logo8 from "./logos/logo8.png";
+
 
 // Export All
 export {
@@ -40,6 +52,17 @@ export {
     texture3,
     texture4,
     texture5,
+    texture6,
+    texture7,
+    texture8,
+    texture9,
+    texture10,
     logo1,
     logo2,
+    logo3,
+    logo4,
+    logo5,
+    logo6,
+    logo7,
+    logo8,
 };
